359de8470d596afcd1bd8da03f3cb997d538fd46
James Walden <james.m.walden@outlook.com>
2024-04-25 15:31:02 +0100

    chore: clean up

--- deploys/k8s/start_k8s_local.sh
+++ deploys/k8s/start_k8s_local.sh
@@ -5,7 +5,10 @@ minikube addons enable ingress
 minikube start --insecure-registry="host.minikube.internal:5001"
 
 # third party
+helm repo add traefik https://traefik.github.io/charts
 helm repo update
+
+helm install traefik traefik/traefik
 
 # install helm charts
 helm upgrade --install frontend ../charts/frontend --values ../charts/frontend/values.yaml --namespace parkrun-club-local --create-namespace
